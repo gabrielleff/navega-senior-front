21af67470deda80561d21687cb169c6da9238982
higorsouza <higor.andrade@hotmail.com>
2023-11-27 17:56:22 -0300

    Adicionando alterações nas configurações

--- navega-senior-front/next.config.js
+++ navega-senior-front/next.config.js
@@ -2,3 +2,26 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+// next.config.js
+// module.exports = {
+//     exportPathMap: function () {
+//       return {
+//         '/': { page: '/' },
+//         // Adicione outras rotas conforme necessário
+//       };
+//     },
+//   };
+
+// // next.config.js
+// module.exports = {
+//     async generateStaticParams() {
+//       // Defina suas rotas aqui
+//       return [
+//         { route: '/' },
+//         // Adicione outras rotas conforme necessário
+//       ];
+//     },
+//   };
+  
+  
